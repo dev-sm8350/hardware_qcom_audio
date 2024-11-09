8857fb678e68874443701ce3d01abbb6d0d4dff9
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-11-09 03:39:48 +0000

    hal: audio: Hardcore some headers path
    Change-Id: 89e9b8cf6813dcd82313f68a8d8554b959030bad
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- post_proc/volume_listener.c
+++ post_proc/volume_listener.c
@@ -41,7 +41,7 @@
 #include <log/log.h>
 #include <hardware/audio_effect.h>
 #include <cutils/properties.h>
-#include <platform_api.h>
+#include <../hal/platform_api.h>
 
 #ifdef __LP64__
 #define PRIMARY_HAL_PATH XSTR(LIB64_AUDIO_HAL)
